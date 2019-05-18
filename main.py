1c364a02a9978a46584681cc3f665f4479f11fd0
GreekCodeCommunity <greekcodecommunity@gmail.com>
2019-05-18 11:36:52 +0300

    Questions general update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helpers import mapt
 def main():
     data = read_csv("./datasets/website-access.data", sep=" ", header=None)
 
-    codes = mapt(lambda d: d[4], data.values)
+    codes = data[4].values
     fivexx = tuple(filter(lambda c: c >= 500 and c < 600, codes))
 
     print(len(fivexx))
