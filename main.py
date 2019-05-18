7c20205aa5800dbbfa5c430d4c7e4ff0c85c983e
sakis <sakispotbullas@gmail.com>
2019-05-19 00:10:18 +0300

    count of ips

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+
 from helpers import append
 from pandas import read_csv
+import pandas as pd
 import Ip2location
 from functools import reduce
-
 def isHour(date, h):
     return True if date.split(":")[1] == h else False
 
@@ -10,7 +11,7 @@ def padToTwo(h):
     return str(h) if h >= 10 else "0" + str(h)
 
 def main():
-    data = read_csv("./datasets/website-access.data", sep=" ", header=None)
+    data = read_csv("datasets/website-access.data", sep=" ", header=None)
 
     codes = data[4].values
     fivexx = tuple(filter(lambda c: c >= 500 and c < 600, codes))
@@ -20,15 +21,18 @@ def main():
     ips = data[0]
     unique_ips = set(ips)
 
-    for i in unique_ips:
-        listofip=i
-        ok=Ip2location.ip2loc(i)
-        print(ok)
+    # for i in unique_ips:
+    #     listofip=i
+    #     ok=Ip2location.ip2loc(i)
+    #     print(ok)
 
-    countries = tuple(zip(tuple(map(Ip2location.ip2loc, unique_ips)), unique_ips))
+    countries = list(zip(list(map(Ip2location.ip2loc, unique_ips)), unique_ips))
     print(countries)
     print(len(ips))
     print(len(unique_ips))
+
+    ok=Ip2location.ip2loc("31.184.238.22")
+    print(ok)
     sum = 0
     requestsPerHour = list(range(24))
     for i in range(24):
