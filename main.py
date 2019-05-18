b5275f7759819ab014dfdf84ccdf58a7c7c15a95
sakis <sakispotbullas@gmail.com>
2019-05-18 14:29:18 +0300

    ip2loc

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from pandas import read_csv
-from helpers import mapt
 
 def main():
     data = read_csv("./datasets/website-access.data", sep=" ", header=None)
 
-    codes = mapt(lambda d: d[4], data.values)
+    codes = data[4].values
     fivexx = tuple(filter(lambda c: c >= 500 and c < 600, codes))
 
-    print(len(fivexx))
+    print("Number of Request: "+str(len(fivexx)))
 
     ips = data[0]
     unique_ips = len(set(ips))
 
-    print(len(ips))
-    print(unique_ips)
+    print("Total traffic: "+str(len(ips)))
+    print("unique IPs: "+str(unique_ips))
+
 
 if __name__ == "__main__":
     main()
+
