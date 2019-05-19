96459712a54077090db810220f30d3c43e859528
Savvas <greekcodecommunity@gmail.com>
2019-05-19 12:09:51 +0300

    Merge branch 'master' of https://github.com/GreekCodeCommunity/gccthon19
    # Conflicts:
    #	main.py

--- ui.py
+++ ui.py
@@ -4,11 +4,25 @@ from tkinter import *
 
 root = tk.Tk()
 root.title('Hackthon 2019')
+root.geometry("500x600")
 
+title_Q_1 = tk.Label(root, text="Γενικά", bg="red", fg="white")
+title_Q_2 = Label(root, text="Something", bg="blue", fg="white")
 
 c1 = Checkbutton(root, text="1. Πόσο ήταν συνολικά το traffic που δέχτηκε ο server;", command=lambda: onClick(1))
 c2 = Checkbutton(root, text="2. Πόσα requests προκάλεσαν 5xx server error;", command=lambda: onClick(2))
 c3 = Checkbutton(root, text="3. Πόσες είναι οι ξεχωριστές IP που επισκέφτηκαν τον server;", command=lambda: onClick(3))
+
+
+def icon():
+    print("ok")
+    photo_pie = PhotoImage(file="Pie.png")
+    label = Label(root, image=photo_pie)
+    label.img=photo_pie
+    label.pack()
+
+
+button_pie = Button(root, text="Το σύνολο των server requests ανά χώρα", command=icon)
 
 
 def onClick(args):
@@ -27,9 +41,12 @@ def onClick(args):
         l2 = Label(text="").pack()
 
 
+title_Q_1.pack(fill=tk.X)
 c1.pack()
 c2.pack()
 c3.pack()
+title_Q_2.pack(fill=tk.X)
+button_pie.pack()
 
 
 root.mainloop()
